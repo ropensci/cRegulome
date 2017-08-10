b3fac5322d4ba5ce61a0658d229cdbb68d66f663
MahShaaban <mahmoud.s.fahmy@students.kasralainy.edu.eg>
2017-08-10 21:53:31 +0900

    reorganize methods

--- R/methods.R
+++ R/methods.R
@@ -39,6 +39,19 @@ print.cmicroRNA <- function(ob, ...) {
  cat(p)
 }
 
+#' @return \code{NULL}
+#' @rdname print
+#' @method print cTF
+#' @export print cTF
+print.cTF <- function(ob, ...) {
+  p <- paste('A cTF object: transcription factor expression correlations in Cancer\n',
+             'Contains:\n',
+             length(ob$studies), 'Cancer study/ies:', paste(head(ob$studies), collapse = ' '), '\n',
+             length(ob$tf), 'Transcription factor/s:', paste(head(ob$TF), collapse = ' '), '\n',
+             length(ob$features), 'features:', paste(head(ob$features), collapse = ' '), '\n')
+  cat(p)
+}
+
 #' Plot method for \link{cmicroRNA} and \link{cTF} objects
 #'
 #' A dot plot of microRNA/TF correlation in a single study of TCGA. When the object \link{cmicroRNA}/\link{cTF}
@@ -91,6 +104,29 @@ plot.cmicroRNA <- function(ob, study = NULL, ...) {
   return(gg)
 }
 
+#' @return \code{NULL}
+#' @rdname plot
+#' @method plot cTF
+#' @export plot cTF
+plot.cTF <- function(ob, study = NULL, ...) {
+  if(length(ob$studies) > 1 && length(study) != 1) {
+    stop('User shouls provide a singl study to plot.')
+  }
+
+  if(is.data.frame(ob$corr)) {
+    dat <- ob$corr
+  } else {
+    dat <- ob$corr[[study]]
+  }
+  dat <- dat %>%
+    gather(tf, cor, -feature) %>%
+    na.omit
+  gg <- dat %>%
+    ggplot(aes(x = tf, y = feature, size = abs(cor))) +
+    geom_point()
+  return(gg)
+}
+
 #' Tidy \link{cmicroRNA} objects
 #'
 #' @inheritParams plot
@@ -120,6 +156,44 @@ tidy.cmicroRNA <- function(ob) {
     dat <- map(ob$corr, function(x) {
       x %>%
         gather(mirna_base, cor, -feature) %>%
+        na.omit
+    }) %>%
+      bind_rows(.id = 'study') %>%
+      select(2:3, study)
+  }
+  return(dat)
+}
+
+#' Tidy \link{cTF} objects
+#'
+#' @inheritParams plot
+#'
+#' @return A tidy \code{data.frame} of four columns. \code{tf} is the official symbol of the gene containin the transcription
+#'         factor, \code{feature} is the features/genes, \code{cor} is the corresponding expression correaltions and \code{study}
+#'          is TCGA study ID.
+#' @examples
+#' \dontrun{
+#' # get data for 2 transcription factors in the ACC study
+#' dat <- get_tf(c('AFF4', 'ESR1'),
+#'  study = c('ACC', 'BLCA'),
+#'  min_cor = .5)
+#'
+#' # convert to cTF object and plot
+#' ob <- cTF(dat)
+#' tidy(ob)
+#' }
+#' @import dplyr tidyr
+#' @export
+tidy.cTF <- function(ob) {
+  if(length(ob$studies) == 1) {
+    dat <- ob$corr %>%
+      gather(tf, cor, -feature) %>%
+      na.omit %>%
+      mutate(study = ob$studies)
+  } else {
+    dat <- map(ob$corr, function(x) {
+      x %>%
+        gather(tf, cor, -feature) %>%
         na.omit
     }) %>%
       bind_rows(.id = 'study') %>%
@@ -163,122 +237,6 @@ venn.diagram.cmicroRNA <- function(ob, study = NULL, ...) {
   venn.diagram(dat, ...)
 }
 
-#' \code{\link[UpSetR]{upset}} plot of microRNA sets
-#'
-#' \code{\link[UpSetR]{upset}} of sets of microRNAs and their correlated features in a TCGA study.
-#'
-#' @inheritParams plot
-#' @return An \code{\link[UpSetR]{upset}} plot
-#' @examples
-#' \dontrun{
-#' # get data for 2 microRNAs in the ACC study
-#' dat <- get_mir(c('hsa-let-7b', 'hsa-mir-134'),
-#'  study = c('ACC', 'BLCA'),
-#'  min_cor = .5)
-#'
-#' # convert to cmicroRNA object and plot
-#' ob <- cmicroRNA(dat)
-#' upset(ob, study = 'ACC')
-#' }
-#' @import dplyr UpSetR
-#' @export
-upset.cmicroRNA <- function(ob, study = NULL, ...) {
-  if(length(ob$studies) > 1 && length(study) != 1) {
-    stop('User shouls provide a singl study to plot.')
-  }
-
-  if(is.data.frame(ob$corr)) {
-    dat <- ob$corr
-  } else {
-    dat <- ob$corr[[study]]
-  }
-
-  dat <- dat %>%
-    mutate_at(vars(2:ncol(dat)),
-              function(x) x <- ifelse(is.na(x), 0, 1))
-  upset(dat, ...)
-}
-
-
-
-
-
-
-
-
-#' @return \code{NULL}
-#' @rdname print
-#' @method print cTF
-#' @export print cTF
-print.cTF <- function(ob, ...) {
-  p <- paste('A cTF object: transcription factor expression correlations in Cancer\n',
-             'Contains:\n',
-             length(ob$studies), 'Cancer study/ies:', paste(head(ob$studies), collapse = ' '), '\n',
-             length(ob$tf), 'Transcription factor/s:', paste(head(ob$TF), collapse = ' '), '\n',
-             length(ob$features), 'features:', paste(head(ob$features), collapse = ' '), '\n')
-  cat(p)
-}
-
-#' @return \code{NULL}
-#' @rdname plot
-#' @method plot cTF
-#' @export plot cTF
-plot.cTF <- function(ob, study = NULL, ...) {
-  if(length(ob$studies) > 1 && length(study) != 1) {
-    stop('User shouls provide a singl study to plot.')
-  }
-
-  if(is.data.frame(ob$corr)) {
-    dat <- ob$corr
-  } else {
-    dat <- ob$corr[[study]]
-  }
-  dat <- dat %>%
-    gather(tf, cor, -feature) %>%
-    na.omit
-  gg <- dat %>%
-    ggplot(aes(x = tf, y = feature, size = abs(cor))) +
-    geom_point()
-  return(gg)
-}
-
-#' Tidy \link{cTF} objects
-#'
-#' @inheritParams plot
-#'
-#' @return A tidy \code{data.frame} of four columns. \code{tf} is the official symbol of the gene containin the transcription
-#'         factor, \code{feature} is the features/genes, \code{cor} is the corresponding expression correaltions and \code{study}
-#'          is TCGA study ID.
-#' @examples
-#' \dontrun{
-#' # get data for 2 transcription factors in the ACC study
-#' dat <- get_tf(c('AFF4', 'ESR1'),
-#'  study = c('ACC', 'BLCA'),
-#'  min_cor = .5)
-#'
-#' # convert to cTF object and plot
-#' ob <- cTF(dat)
-#' tidy(ob)
-#' }
-#' @import dplyr tidyr
-#' @export
-tidy.cTF <- function(ob) {
-  if(length(ob$studies) == 1) {
-    dat <- ob$corr %>%
-      gather(tf, cor, -feature) %>%
-      na.omit %>%
-      mutate(study = ob$studies)
-  } else {
-    dat <- map(ob$corr, function(x) {
-      x %>%
-        gather(tf, cor, -feature) %>%
-        na.omit
-    }) %>%
-      bind_rows(.id = 'study') %>%
-      select(2:3, study)
-  }
-}
-
 #' Venn Diagram of transcription factors correlated features
 #'
 #' Count and plot the numbers of transcription factors correlated features in \code{cTF} object.
@@ -313,6 +271,42 @@ venn.diagram.cTF <- function(ob, study = NULL, ...) {
     na.omit
   dat <- with(dat, split(feature, tf))
   venn.diagram(dat, ...)
+}
+
+#' \code{\link[UpSetR]{upset}} plot of microRNA sets
+#'
+#' \code{\link[UpSetR]{upset}} of sets of microRNAs and their correlated features in a TCGA study.
+#'
+#' @inheritParams plot
+#' @return An \code{\link[UpSetR]{upset}} plot
+#' @examples
+#' \dontrun{
+#' # get data for 2 microRNAs in the ACC study
+#' dat <- get_mir(c('hsa-let-7b', 'hsa-mir-134'),
+#'  study = c('ACC', 'BLCA'),
+#'  min_cor = .5)
+#'
+#' # convert to cmicroRNA object and plot
+#' ob <- cmicroRNA(dat)
+#' upset(ob, study = 'ACC')
+#' }
+#' @import dplyr UpSetR
+#' @export
+upset.cmicroRNA <- function(ob, study = NULL, ...) {
+  if(length(ob$studies) > 1 && length(study) != 1) {
+    stop('User shouls provide a singl study to plot.')
+  }
+
+  if(is.data.frame(ob$corr)) {
+    dat <- ob$corr
+  } else {
+    dat <- ob$corr[[study]]
+  }
+
+  dat <- dat %>%
+    mutate_at(vars(2:ncol(dat)),
+              function(x) x <- ifelse(is.na(x), 0, 1))
+  upset(dat, ...)
 }
 
 #' \code{\link[UpSetR]{upset}} plot of transcription factors' sets
@@ -350,4 +344,3 @@ upset.cTF <- function(ob, study = NULL, ...) {
               function(x) x <- ifelse(is.na(x), 0, 1))
   upset(dat, ...)
 }
-
